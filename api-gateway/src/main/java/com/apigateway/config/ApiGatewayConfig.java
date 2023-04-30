b675b52682b6bd00b530908569ee92f4c234c3af
Pablo Magalhães <109962972+pabloaugustocm17@users.noreply.github.com>
2023-04-30 14:37:54 -0300

    UPDATE
    - Implementação de gateway
    - Configuração de rotas do gateway

---
+++ api-gateway/src/main/java/com/apigateway/config/ApiGatewayConfig.java
@@ -0,0 +1,36 @@
+package com.apigateway.config;
+
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.Buildable;
+import org.springframework.cloud.gateway.route.builder.PredicateSpec;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class ApiGatewayConfig {
+
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder){
+
+
+        return builder.routes()
+                .route
+                        (p ->   p.path("/book-service/**")
+                                .uri("lb://book-service")
+                        )
+                .route
+                        (p ->   p.path("/cambio-service/**")
+                                .uri("lb://cambio-service")
+                        )
+                .build();
+    }
+
+}
+
+//.route( p -> p.path("/get")
+//                        .filters(f ->   f.addRequestHeader("Hello", "World")
+//                                .addRequestParameter("hello", "world"))
+//                        .uri("http://httpbin.org:80"))
